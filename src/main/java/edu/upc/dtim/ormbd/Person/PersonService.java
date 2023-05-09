ebc2d9a14c0e51e6d3067f76c7b800f2254af77f
Víctor Asenjo <victorasenj@gmail.com>
2023-05-09 11:33:49 +0200

    ORM with XML working

--- src/main/java/edu/upc/dtim/ormbd/Person/PersonService.java
+++ src/main/java/edu/upc/dtim/ormbd/Person/PersonService.java
@@ -9,38 +9,26 @@ import javax.persistence.Persistence;
 import java.util.List;
 @Service
 public class PersonService {
+
     public Person savePerson(Person person) {
         Person yo = person;
+        person.setName("Asenjo");
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("UnidadPersonas");
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
-
+            em.persist(person); // persist the person object
             em.getTransaction().commit();
+            System.out.println("VALORES INSERTADOS --------------------------------------");
+
         } catch (Exception e) {
+            System.out.println("ERRORRRR --------------------------------------");
             e.printStackTrace();
         } finally {
             em.close();
         }
-
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("UnidadPersonas");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-        try {
-            entityManager.getTransaction().begin();
-            entityManager.persist(yo);
-            entityManager.getTransaction().commit();
-            System.out.println("VALORES INSERTADOS --------------------------------------");
-        } catch (Exception e) {
-            System.out.println("ERRORRRR --------------------------------------");
-            e.printStackTrace();
-
-        } finally {
-            entityManager.close();
-        }
-
         System.out.println("FIN MAIN --------------------------------------");
 
-        return yo;
+        return person;
     }
 }
