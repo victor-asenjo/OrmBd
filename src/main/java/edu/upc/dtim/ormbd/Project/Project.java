80316fba18b948fd38abe2745b7695e1343ee9f2
Víctor Asenjo <victorasenj@gmail.com>
2023-05-09 15:46:58 +0200

    WIP orm ordenando clases i añadiendo atributos WORKING

--- src/main/java/edu/upc/dtim/ormbd/Project/Project.java
+++ src/main/java/edu/upc/dtim/ormbd/Project/Project.java
@@ -1,16 +1,79 @@
 package edu.upc.dtim.ormbd.Project;
 
-public class Project {
 
+import java.util.List;
+
+public class Project {
     public String getProjectId() {
         return projectId;
     }
 
-    public void setProjectId(String name) {
-        this.projectId = name;
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
     }
 
+    // Unique identifier for the project
     private String projectId;
+
+    // Name of the project
+    private String projectName;
+
+    // Description of the project
+    private String projectDescription;
+
+    // Privacy level of the project (e.g. private, public)
+    private String projectPrivacy;
+
+    // Color associated with the project (not sure what this is used for)
+    private String projectColor;
+
+    // Username of the user who created the project
+    private String createdBy;
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+
+    public String getProjectPrivacy() {
+        return projectPrivacy;
+    }
+
+    public void setProjectPrivacy(String projectPrivacy) {
+        this.projectPrivacy = projectPrivacy;
+    }
+
+    public String getProjectColor() {
+        return projectColor;
+    }
+
+    public void setProjectColor(String projectColor) {
+        this.projectColor = projectColor;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+
+
+    // List of local graph IDs associated with the project (not sure what this is used for)
+    //private List<String> localGraphIDs;
 
     // constructor, getters, setters, etc.
 }
